f508abe87e0b051827603484af6322cd76ac330f
Locietta <locietta@qq.com>
2024-04-23 15:27:52 +0800

    chore(type): fix typing for opencv

--- resources/resource.py
+++ resources/resource.py
@@ -1,8 +1,9 @@
 import cv2
+import numpy as np
 
-img = cv2.imread(R"resources/diffuse.jpg")[64:64+128,64:64+128,[2,1,0]]/255
-normal = cv2.imread(R"resources/normal.jpg")[64:64+128,64:64+128,[2,1,0]]/255
-roughness = cv2.imread(R"resources/roughness.jpg")[64:64+128,64:64+128,[2,1,0]]/255
+img = np.divide(cv2.imread(R"resources/diffuse.jpg")[64:64+128,64:64+128,[2,1,0]], 255)
+normal = np.divide(cv2.imread(R"resources/normal.jpg")[64:64+128,64:64+128,[2,1,0]], 255)
+roughness = np.divide(cv2.imread(R"resources/roughness.jpg")[64:64+128,64:64+128,[2,1,0]], 255)
 roughness *= roughness
 
 from utils.mesh import Mesh
